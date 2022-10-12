116c41adfecb6496904d4970b10b3f99c68fee11
okb-777 <bartekkszke2@gmail.com>
2022-10-12 14:27:31 +0200

    pod strona

---
+++ wynik.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Wyniki</title>
+        <link rel="stylesheet" href="styl.css">
+    </head>
+    <body>
+        <div class="banner">
+            <h2>Twój wynik</h2>
+            <h5>tu powinien być wynik</h5>
+        </div>
+        <div class="main">
+            <h5>tu coś może się da</h5>
+        </div>
+        <div class="footer">
+            <a href="index.php"><button type="submit" class="button">powrót</button></a>
+        </div>
+    </body>
+</html>
